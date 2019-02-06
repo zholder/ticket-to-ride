e8284729ed27445238aa834a833395427f7db61f
Zach Holder <zacharysholder@gmail.com>
2019-02-06 17:25:13 -0600

    initial commit

---
+++ ticket-to-ride/src/main/java/com/zachholder/tickettoride/TicketToRideApplication.java
@@ -0,0 +1,14 @@
+package com.zachholder.tickettoride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketToRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketToRideApplication.class, args);
+	}
+
+}
+
